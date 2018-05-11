0455cd6c92731089f2d724317a4ddc0186d2b055
minzhaoxie <36650553+minzhaoxie@users.noreply.github.com>
2018-05-11 17:20:44 -0400

    updated

--- huobi.py
+++ huobi.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
-#author: 半熟的韭菜
+#author: Max
 
 from websocket import create_connection
 import gzip
 import time
 import os,os.path
 import datetime
-import json
-import csv
 #from multiprocessing import Process
 import threading
 
@@ -15,9 +13,11 @@ class huobi(threading.Thread):
     MAX_SIZE = 50000000
     def __init__(self,symbols,mType):
         threading.Thread.__init__(self)
+        #Process.__init__(self)
         self.symbols = symbols
         self.mType = mType
         date = self.getDateStr()
+        self.mkdir(date)
         self.NUMBER = len([name for name in os.listdir(date) if os.path.isfile(os.path.join(date, name))])
     def mkdir(self,path):
         '''
@@ -39,7 +39,6 @@ class huobi(threading.Thread):
     def getPath(self):
         date = self.getDateStr()
         self.mkdir(date)
-        
         if(self.NUMBER < 10):
             path = date+'/huobi_'+'_'+self.mType+'_0'+str(self.NUMBER)+'.txt'
         else:
@@ -105,138 +104,36 @@ class huobi(threading.Thread):
                 ws.send(tradeStr)
             else:
                 print('mType incorrect')
+
         while(1):
-            compressData=ws.recv()
-            result=gzip.decompress(compressData).decode('utf-8')
-            if result[:7] == '{"ping"':
-                ts=result[8:21]
-                pong='{"pong":'+ts+'}'
-                ws.send(pong)
-                ws.send(tradeStr)
-            else:
-                #print(result)
-                self.writeToJson(result)
-            #time.sleep(1)
-
-
-class huobiConverter(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-        now = str(datetime.datetime.now())
-        self.date = str(now)[:10]
-        
-    def mkdir(self,path):
-        '''
-        防止目录存在
-        '''
-        if not os.path.exists(path):
-            os.mkdir(path)
-            
-    def addBracket(self,filename):
-        print('\n workind on : \n')
-        print(filename)
-        with open(filename, 'r+') as f:
-            content = f.read()
-            with open(self.date+'/tmp.txt', 'w') as t:
-                if(content[0] == "{"):
-                    t.seek(0, 0)
-                    t.write('[' + content+']')
+            try:
+                compressData=ws.recv()
+                result=gzip.decompress(compressData).decode('utf-8')
+                if result[:7] == '{"ping"':
+                    ts=result[8:21]
+                    pong='{"pong":'+ts+'}'
+                    ws.send(pong)
+                    ws.send(tradeStr)
                 else:
-                    t.write(content)
-                t.close()
-            f.close()
-            
-    def readJson(self,filename):
-        path = self.date+'/'+filename
-        self.addBracket(path)
-        with open(self.date+'/tmp.txt') as json_file:
-            data = json.load(json_file)
-            self.writeToCsv(filename,data)
-            json_file.close()
-        os.remove(self.date+'/tmp.txt')
-        
-            
-               
-    def writeToCsv(self,filename, datas):
-        for data in datas:
-            if('id' not in data):
-                ch = data['ch']
-                if('depth' in ch):
-                    symbol = ch.split(".")[1]
-                    ts = data['ts']
-                    line = []
-                    line.append(symbol)
-                    line.append(ts)
-                    line.append('bid')
-                    for bs in data['tick']['bids']:
-                        for bid in bs:
-                            line.append(bid)  
-                            
-                    line.append('ask')
-                    for ass in data['tick']['asks']:
-                        for ask in ass:
-                            line.append(ask)
-                    
-                    csvFile = self.getPath(symbol,'market_depth')
-                    with open(csvFile, "a", newline='') as csv_file:
-                        writer = csv.writer(csv_file) 
-                        writer.writerow(line)
-                elif('trade.detail' in ch):
-                    symbol = ch.split(".")[1]
-                    ts = data['ts']
-                    for trade in data['tick']['data']:
-                        line = []
-                        ts = trade['ts']
-                        amount = trade['amount']
-                        price = trade['price']
-                        direction = trade['direction']
-                        line.append(symbol)
-                        line.append(ts)
-                        line.append(direction)
-                        line.append(price)  
-                        line.append(amount) 
-                        csvFile = self.getPath(symbol,'trade_detail')
-                        with open(csvFile, "a", newline='') as csv_file:
-                            writer = csv.writer(csv_file) 
-                            writer.writerow(line)
-
-                    
-    
-    def getPath(self,symbol,mType):
-        #csvFile = self.symbol + '/'+filename.split('_')[0] + '_huobi_'+self.symbol+'_'+self.mType+'.csv'
-        self.mkdir('output')
-        csvFile = 'output/'+self.date + '_huobi_'+symbol+'_'+mType+'.csv'
-        print(csvFile)
-        return csvFile   
-    
-    def run(self):
-        for filename in os.listdir(self.date):
-            self.readJson(filename)             
+                    #print(result)
+                    self.writeToJson(result)
+                #time.sleep(1)
+            except:
+                print("\n\n Error occured, disconnected!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \n")
+                self.run()
+                break
+       
                 
 if __name__ == '__main__':
     downloadThreads = []   
-    convertThreads = []
     symbols = ['ethusdt','btcusdt','bchusdt','etcusdt','ltcusdt','eosusdt','xrpusdt','omgusdt','dashusdt','zecusdt','iotausdt','adausdt','steemusdt','bchbtc','ethbtc','ltcbtc','etcbtc','eosbtc','omgbtc','xrpbtc','dashbtc','zecbtc','iotabtc','adabtc','steembtc']
     marketDepth = 'market_depth'
     tradeDetail = 'trade_detail'
     
-    a=huobi(symbols, marketDepth)
-    b=huobi(symbols, tradeDetail)
-
     
     downloadThreads.append(huobi(symbols, marketDepth))
     downloadThreads.append(huobi(symbols, tradeDetail))
-    convertThreads.append(huobiConverter())
     
-# =============================================================================
-#     for dt in downloadThreads:
-#         dt.start()
-# =============================================================================
+    for dt in downloadThreads:
+        dt.start()
         
-    for ct in convertThreads:
-        ct.start()
-    
-    for ct in convertThreads:
-        ct.join()
-        
-
